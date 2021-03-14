fb9a188cde97dca7af5e647af80ee338254b4893
James Verley <dyseterboy@gmail.com>
2021-03-14 23:15:50 +1100

    added option to receive compressed

--- transfer_files.sh
+++ transfer_files.sh
@@ -10,8 +10,7 @@ then
 	while ! [[ -d "transfer_files" ]]
 	do
 		mkdir transfer_files
-		echo "place files to be sent in the 'transfer_files' directory"
-		echo "Enter to continue"
+		echo "place files to be sent in the 'transfer_files' directory\nEnter to continue"
 		read
 	done
 	
@@ -19,13 +18,23 @@ then
 	read address
 	tar -czvf - transfer_files | pv | netcat $address $port
 		
-else
-        echo "waiting/receiving"
-        file_suffix=0
-        while [[ -d "received"+$file_suffix+".tar.gz" ]]
+else	
+	echo "receive decompressed? yes/no"
+	read compression
+	
+	echo "waiting/receiving"
+	file_suffix=0
+        while [[ -d "received"$file_suffix ]]
         do
                 file_suffix=$(($file_suffix+1))
-        done
-	cd "received$file_suffix"
-        netcat -l -p $port | pv | tar -xzvf -
+	done
+	
+	if ! [[ $compression = 'yes' ]]
+	then
+		netcat -l -p $port | pv | tar -xzvf -
+	else
+	        mkdir "received$file_suffix"
+		cd "received$file_suffix"
+		netcat -l -p $port | pv
+	fi
 fi
