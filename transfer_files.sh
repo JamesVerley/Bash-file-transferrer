8269f58df3c07dd82dd9ff02ac56a42654456579
JamesVerley <dyseterboy@gmail.com>
2021-03-16 22:57:13 +1100

    Fix syntax

--- transfer_files.sh
+++ transfer_files.sh
@@ -1,40 +1,47 @@
-echo "current directory '"$(pwd)"'"                                                                               
+#!/bin/bash
+
+echo "current directory '"$(pwd)"'"
 echo "enter port"
 read port
 
 echo "send? yes/no"
 read send
 
-if [[ $send = 'yes' ]];
+
+
+
+if [ $send = "yes" ]
 then
-	while ! [[ -d "transfer_files" ]]
+	echo yes
+	while ! [ -d "transfer_files" ]
 	do
 		mkdir transfer_files
 		echo "place files to be sent in the 'transfer_files' directory\nEnter to continue"
 		read
 	done
-	
+
 	echo "Enter address"
 	read address
 	tar -czvf - transfer_files | pv | netcat $address $port
-		
-else	
-	echo "receive decompressed? yes/no"
+
+else
+	echo no
+	echo "receive compressed? yes/no"
 	read compression
-	
+
 	echo "waiting/receiving"
 	file_suffix=0
-        while [[ -d "received"$file_suffix ]]
-        do
-                file_suffix=$(($file_suffix+1))
+	while [ -d "received$file_suffix" ] || [ -f "received$file_suffix.tar.gz" ]
+	do
+		file_suffix=$(($file_suffix+1))
 	done
-	
-	if ! [[ $compression = 'yes' ]]
+
+	if [ $compression = "no" ]
 	then
+		mkdir "received$file_suffix"
+		cd "received$file_suffix"
 		netcat -l -p $port | pv | tar -xzvf -
 	else
-	        mkdir "received$file_suffix"
-		cd "received$file_suffix"
-		netcat -l -p $port | pv
+		netcat -l -p $port | pv > "received$file_suffix.tar.gz"
 	fi
 fi
